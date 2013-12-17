95df0202477228fa38371e7303a7083ee3197c1a
Daniel Morris <daniel.morris@answerconsulting.com>
2013-12-17 13:01:33 +0000

    updated assembly

--- Answer.Emailer/Properties/AssemblyInfo.cs
+++ Answer.Emailer/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Answer.Emailer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Answer Consulting")]
 [assembly: AssemblyProduct("Answer.Emailer")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
